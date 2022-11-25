281372709654a163b17ff828a93b582afdc882c1
Miguel <miguelantonio.otoya@unmsm.edu.pe>
2022-11-25 00:19:58 -0500

    Avance hastas la segunda aula

---
+++ index.js
@@ -0,0 +1,20 @@
+/*Importación de clases*/
+import {Cliente} from './Cliente.js'
+import {CuentaCorriente} from './CuentaCorriente.js';
+import {CuentaAhorro} from './CuentaAhorro.js';
+
+
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+const cliente2 = new Cliente('María','16979808','8989');
+
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+
+const CuentaAhorroLeonardo = new CuentaAhorro(cliente,'9985','001',0);
+console.log(cuentaDeLeonardo);
+console.log(CuentaAhorroLeonardo);
+
+
+
+
